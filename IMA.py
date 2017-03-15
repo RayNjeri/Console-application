e6d22f2e53ddc8c31a02cc6e24669cce9b5c5632
Rachael Njeri <njerirachael0@gmail.com>
2017-03-15 14:30:11 +0300

    building a relationship'

--- IMA.py
+++ IMA.py
@@ -1,5 +1,7 @@
 import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import session
 import datetime
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,20 +9,30 @@ Base = declarative_base()
 
 from sqlalchemy import Table, Column, Integer, String, Boolean, DateTime, Float, ForeignKey
 
-class InventoryModel(Base):
+class CategoryModel(Base):
 	__tablename__ = "Category"
+	category_id = Column(Integer, primary_key=True)
+	name = Column(String)
+	details = Column(String)
+	Item = relationship("Item", back_populates ="category")
+
+	def __str__(self):
+		return 'CategoryModel(category_id={},name ={}, details={})'.format(self.category_id, self.details)
+
+class ItemModel(Base):
+	__tablename__ = "Item"
 	item_id = Column(Integer, primary_key=True)
 	name = Column(String)
 	details = Column(String)
-	quantity = Column(String)
 	unit_price = Column(Float)
 	total = Column(Float)
-
-	__tablename__ = "Item"
-	serial_id =Column(Integer, primary_key=True)
-	item_id = Column(Integer)
 	date_added = Column(DateTime,default=datetime.datetime.now)
 	status = Column(Boolean)
+	category_id = Column(Integer, ForeignKey('Category.category_id'))
+	CategoryModel = relationship("Category", back_populates ="Item")
+
+	def __str__(self):
+		return 'ItemModel(item_id={}, name={}, details={}, unit_price={}, total={}, date_added={}, status={})'.format(self.item_id, self.name, self.details, self.unit_price, self.total, self.date_added, self.status)
 
 engine = create_engine('sqlite:///inventory.db')
 Base.metadata.create_all(engine)
