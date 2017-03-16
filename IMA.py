ad99f44175fca409306a1299c39586c0cef683a1
Rachael Njeri <njerirachael0@gmail.com>
2017-03-16 18:06:30 +0300

    clean code

--- IMA.py
+++ IMA.py
@@ -11,8 +11,8 @@ from sqlalchemy import Table, Column, Integer, String, Boolean, DateTime, Float,
 
 class CategoryModel(Base):
 	__tablename__ = "category"
-	category_id = Column(Integer)
-	name = Column(String, primary_key=True)
+	id = Column(Integer, primary_key = True)
+	name = Column(String, unique = True)
 	details = Column(String)
 	itemModel = relationship("ItemModel", backref ="category")
 
@@ -21,7 +21,7 @@ class CategoryModel(Base):
 
 class ItemModel(Base):
 	__tablename__ = "Item"
-	item_id = Column(Integer, primary_key=True)
+	id = Column(Integer, primary_key=True)
 	name = Column(String)
 	details = Column(String)
 	unit_cost = Column(Float)
@@ -29,13 +29,13 @@ class ItemModel(Base):
 	total = Column(Float)
 	date_added = Column(DateTime,default=datetime.datetime.now)
 	status = Column(Boolean)
-	category_id = Column(Integer, ForeignKey('category.name'))
+	category_id= Column(Integer, ForeignKey('category.id'))
 	categoryModel = relationship("CategoryModel", backref ="Item")
 
 	def __str__(self):
 		return 'ItemModel(item_id={}, name={}, details={}, unit_cost={}, quantity={}, total={}, date_added={}, status={})'.format(self.item_id, self.name, self.details, self.unit_cost, self.quantity, self.total, self.date_added, self.status)
 
-engine = create_engine('sqlite:///inventory.db')
+engine = create_engine('sqlite:///newInventory.db')
 Base.metadata.create_all(engine)
 Base.metadata.bind = engine 
 
