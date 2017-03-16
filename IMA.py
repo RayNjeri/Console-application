c72e45ed24edfef3a51f900fb14b8b5371c65ff1
Rachael Njeri <njerirachael0@gmail.com>
2017-03-16 17:18:10 +0300

    fix error and clean code

--- IMA.py
+++ IMA.py
@@ -10,11 +10,11 @@ Base = declarative_base()
 from sqlalchemy import Table, Column, Integer, String, Boolean, DateTime, Float, ForeignKey
 
 class CategoryModel(Base):
-	__tablename__ = "Category"
-	category_id = Column(Integer, primary_key=True)
-	name = Column(String)
+	__tablename__ = "category"
+	category_id = Column(Integer)
+	name = Column(String, primary_key=True)
 	details = Column(String)
-	Item = relationship("Item", back_populates ="category")
+	itemModel = relationship("ItemModel", backref ="category")
 
 	def __str__(self):
 		return 'CategoryModel(category_id={},name ={}, details={})'.format(self.category_id, self.details)
@@ -24,15 +24,16 @@ class ItemModel(Base):
 	item_id = Column(Integer, primary_key=True)
 	name = Column(String)
 	details = Column(String)
-	unit_price = Column(Float)
+	unit_cost = Column(Float)
+	quantity =Column(Float)
 	total = Column(Float)
 	date_added = Column(DateTime,default=datetime.datetime.now)
 	status = Column(Boolean)
-	category_id = Column(Integer, ForeignKey('Category.category_id'))
-	CategoryModel = relationship("Category", back_populates ="Item")
+	category_id = Column(Integer, ForeignKey('category.name'))
+	categoryModel = relationship("CategoryModel", backref ="Item")
 
 	def __str__(self):
-		return 'ItemModel(item_id={}, name={}, details={}, unit_price={}, total={}, date_added={}, status={})'.format(self.item_id, self.name, self.details, self.unit_price, self.total, self.date_added, self.status)
+		return 'ItemModel(item_id={}, name={}, details={}, unit_cost={}, quantity={}, total={}, date_added={}, status={})'.format(self.item_id, self.name, self.details, self.unit_cost, self.quantity, self.total, self.date_added, self.status)
 
 engine = create_engine('sqlite:///inventory.db')
 Base.metadata.create_all(engine)
