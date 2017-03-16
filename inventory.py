e3f05d5f9d80467cab21981ee245a9e7164ad01e
Rachael Njeri <njerirachael0@gmail.com>
2017-03-16 17:16:46 +0300

    fix errors

--- inventory.py
+++ inventory.py
@@ -1,31 +1,27 @@
 from IMA import CategoryModel, ItemModel
 from sqlalchemy.orm import Session
+import datetime
 session = Session()
 
 class Products:
-    def __init__(self, name, details):
-        self.name = name
-        self.details = details
-
-    
-    def get_Item_Category():
+    def get_Item_Category(self):
       categories= sesssion.query(CategoryModel).all()
       for category in categories:
         print("{} {}".format(category.name, category.details))
         category_id = input('Which Category?')
       return int(category_id)
 
-    def create_category(self, name, details):
-        category = CategoryModel(name=name, details=details)
+    def create_category(self, category_id, name, details):
+        category = CategoryModel(category_id=category_id, name=name, details=details)
         session.add(category)
         session.commit()
     
-    def update_category(self, name, detail):
+    def update_category(self, name):
        categories= session.query(CategoryModel).all()
        print('select which category to update')
-       category_id = get_Item_Category
+       category_id = get_Item_Category()
        category = input('enter field to update')
-       category_id = session.query.filter_by(category_id= category_id).first
+       category_id = session.query.filter_by(name= name).one()
        if not category:
          print('Category not found')
        else:
@@ -34,12 +30,12 @@ class Products:
         session.commit()
         print('Category successfully updated.')
 
-    def delete_category(name, details):
+    def delete_category(self, name, details):
       categories= session.query(CategoryModel).all()
       print('select which category to Delete')
-      category_id = get_Item_Category
+      category_id = get_Item_Category()
       category = input('enter field to Delete')
-      category_id = session.query.filter_by(category_id= category_id).first
+      category_id = session.query.filter_by(name= name).one()
       if not category:
          print('Category not found')
       else:
@@ -47,25 +43,16 @@ class Products:
         session.commit()
         print('Category successfully deleted.')
 
-    def view_category(name,details):
-        print('select which category to view')
-        for instance in session.query(category).order_by(category_id, category_name, category_details):
-            print(instance.category_id, instance.name, instance.details)
+    def view_category(self, name,details):
+        category = session.query(CategoryModel).filter_by(name=name).one();
+        print(category)
 
-    def search_category(name,details):
-        print('select which category to view')
-        for instance in session.query(category).order_by(category_id, category_name, category_details):
-            print(instance.category_id, instance.name, instance.details)
+    def search_category(self, name,details):
+        category = session.query(CategoryModel).filter_by(name=name).one();
+        print(category)
     
-class Particulars():
-    def __init__(self, name, details, unit_cost, total, quantity, status, date_added, category_id):
-        self.name = name
-        self.details = details
-        self.unit_cost = unit_cost
-        self.quantity = quantity
-        self.date_added = date_added
-        self.category_id = category_id
 
+class Particulars:
     def get_items_Item():
         items= sesssion.query(CategoryModel).all()
         for item in categories:
@@ -73,17 +60,17 @@ class Particulars():
             item_id = input('Which item?')
             return int(item_id)
 
-    def create_item(self, name, details, unit_cost, quantity, total, status, date_added):
-        item = CategoryModel(name=name, details=details, unit_cost =unit_cost, quantity =quantity, status =status, date_added =date_added)
-        session.add(category)
+    def create_item(self, name, details, unit_cost, quantity, total, status, date_added, category_id):
+        item = ItemModel(name=name, details=details, unit_cost =unit_cost, quantity =quantity, total =total, status =status, date_added =date_added, category_id =category_id)
+        session.add(item)
         session.commit()
 
-    def update_item(self, name, details, unit_cost,quantity, total, status, date_added):
+    def update_item(self, name):
        items= session.query(CategoryModel).all()
        print('select which item to update')
        item_id = get_items_Item
        item = input('enter field to update')
-       item_id = session.query.filter_by(item_id= item_id).first
+       item_id = session.query.filter_by(name= name).one()
        if not item:
          print('item not found')
        else:
@@ -92,12 +79,12 @@ class Particulars():
         session.commit()
         print('item successfully updated.')
 
-    def delete_item(self, name, details, unit_cost, quantity, total, status, date_added):
-        categories= session.query(CategoryModel).all()
-        print('select which category to Delete')
+    def delete_item(self, name):
+        item= session.query(ItemModel).all()
+        print('select which item to Delete')
         item_id = get_items_Item
         item = input('enter field to Delete')
-        item_id = session.query.filter_by(item_id= item_id).first
+        item_id = session.query.filter_by(name= name).one()
         if not item:
             print('item not found')
         else:
@@ -105,18 +92,24 @@ class Particulars():
             session.commit()
             print('item successfully deleted.')
 
-    def view_item(name,details, unit_cost,quantity, total,status,date_added):
-        print('select which item to view')
-        for instance in session.query(item).order_by(item_id, item_name, item_details, item_unit_cost, item_quantity, item_total, item_status):
-            print(instance.item_id, instance.name, instance.details, instance.unit_cost, instance.quantity, instance.total, instance.status)
+    def view_item(self, name,details, unit_cost,quantity, total,status,date_added):
+        item = session.query(ItemModel).filter_by(name=name).one();
+        print(item)
 
-    def search_item(name,details, unit_cost, quantity, total,status,date_added):
-        print('select which item to view')
-        for instance in session.query(item).order_by(item_id, item_name, item_details, item_unit_cost, item_quantity, item_total, item_status):
-            print(instance.item_id, instance.name, instance.details, instance.unit_cost, instance.quantity, instance.total, instance.status)
+    def search_item(self, name):
+        item = session.query(ItemModel).filter_by(name=name).one();
+        print(item)
      
-     def assert_value()
+    def asset_value(self, quantity, unit_cost):
+        items= sesssion.query(CategoryModel).all()
+        x = session.query.filter_by(name = name).first
+        return x
+        y = session.query.filter_by(unit_cost= unit_cost).first
+        return y
+        asset_value = item.total
+        total = x*y
+        return total 
+
+
       
-y = Products("phone", "samsung")
-y.create_category("phone", "samsung")      
-        
+
